629da6bfe43678053cf3db2124dd047204b52343
Daniel Kao <daniel.kao@linecorp.com>
2016-12-30 17:06:13 +0800

    fix image preview issue

--- image_management.py
+++ image_management.py
@@ -1,6 +1,11 @@
-from cloudinary import uploader
+from cloudinary import uploader, CloudinaryImage
+
 
 def upload(user_id, file_name):
     response = uploader.upload(file_name, tags=user_id)
     print response
     return response['secure_url'], response['public_id']
+
+def getPreviewImage(imageId):
+    return CloudinaryImage(imageId).build_url(width=240)
+
